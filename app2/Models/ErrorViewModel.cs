8117118e73f92a4c77b6bffef774fd3f7fc1d88e
mrkyaing <mr.kyaing7@gmail.com>
2023-06-17 12:40:58 +0630

    remove apps

--- app2/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace app2.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
