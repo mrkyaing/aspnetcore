cbcd532f1dae2dcbb99c1e749c8a117bb44d6782
mrkyaing <mr.kyaing7@gmail.com>
2023-07-15 14:41:08 +0630

    remove duplicate project .

--- ExecriseCoreMVC/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-//register the controllers and views folder(s) to know the urls 
-builder.Services.AddControllersWithViews();
-var app = builder.Build();
-//mapping the routes to know via the browser https://www.facebook/profile/me
-app.MapControllerRoute(name:"default",pattern: "{controller=Execrise}/{action=Index}/{id?}");
-app.Run();
