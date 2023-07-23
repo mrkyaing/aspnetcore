d7428145788240f1f270b0e680d5b5924f7c4e86
mrkyaing <mr.kyaing7@gmail.com>
2023-07-23 13:13:57 +0630

    product crud functions

--- RestaurantManagementSystem/Controllers/ProductController.cs
+++ RestaurantManagementSystem/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using RestaurantManagementSystem.Models;
 using RestaurantManagementSystem.Models.ViewModels;
 using System.Net.Sockets;
 using System.Net;
+using Microsoft.EntityFrameworkCore;
 
 namespace RestaurantManagementSystem.Controllers {
     public class ProductController : Controller {
@@ -13,19 +14,31 @@ namespace RestaurantManagementSystem.Controllers {
         {
            rMSDBContext = context;
         }
-        public IActionResult Index() {
-            return View();
+        public IActionResult List() {
+            int i = 0;
+          IList<ProductViewModel> products = rMSDBContext.Products.Select(x => new ProductViewModel
+          //data exchange between View Model and Model >> DTO  
+          {
+               SrNo=i,
+                Id = x.Id,
+                Name = x.Name,
+                Code = x.Code,
+                UnitPrice = x.UnitPrice,
+                Category = x.Category,
+                IsAvailable = x.IsAvailable == true ? "Yes" : "No",
+                IsTodaySpecial = x.IsTodaySpecial == true ? "Yes" : "No"
+            }).OrderBy(o=>o.Code).ToList();
+            return View(products);
         }
 
         public IActionResult Entry()=>View();
         [HttpPost]
         public IActionResult Entry(ProductViewModel productViewModel) {
             try {
-              
-                Product product = new Product()
-                {
-                    Id = Guid.NewGuid().ToString(),//for new id when uer create the record 36 char
-                    Name = productViewModel.Name,//c101
+                //DTO >> Data Transfer Object 
+                Product product = new Product(){
+                    Id = Guid.NewGuid().ToString(),//for new id when uer create the record 36 char GUID  , UUID 
+                    Name = productViewModel.Name,//c101 
                     Code = productViewModel.Code,
                     UnitPrice = productViewModel.UnitPrice,
                     IsAvailable = productViewModel.IsAvailable.Equals("y")?true : false,
@@ -42,6 +55,62 @@ namespace RestaurantManagementSystem.Controllers {
             return View();
         }
 
+        public IActionResult Delete(string Id) {
+            try {
+                Product product = rMSDBContext.Products.Where(x => x.Id.Equals(Id)).SingleOrDefault();
+                if (product == null) {
+                    TempData["Msg"] = "There is no recrod that you select.";
+                }
+                rMSDBContext.Products.Remove(product);// collect the data to remove
+                rMSDBContext.SaveChanges();// remove the record from the database 
+                TempData["Msg"] = "delete process is completed successfully.";
+            }
+            catch(Exception e) {
+                TempData["Msg"] = "error occur when record is deleted.";
+            }
+            return RedirectToAction("List");
+
+        }
+
+        public IActionResult Edit(string Id) {
+            ProductViewModel  productViewModel = rMSDBContext.Products.Where(x => x.Id.Equals(Id)).Select(x=>new ProductViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Code = x.Code,
+                UnitPrice = x.UnitPrice,
+                Category = x.Category,
+                IsAvailable = x.IsAvailable == true ? "y" : "n",
+                IsTodaySpecial = x.IsTodaySpecial == true ? "y" : "n"
+            }).SingleOrDefault();
+
+            return View(productViewModel);
+        }
+        [HttpPost]
+        public IActionResult Update(ProductViewModel productViewModel) {
+            try {
+                //DTO >> Data Transfer Object 
+                Product product = new Product()
+                {
+                    Id =productViewModel.Id,//not to generate new id because this is update processs 
+                    Name = productViewModel.Name,//c101
+                    Code = productViewModel.Code,
+                    UnitPrice = productViewModel.UnitPrice,
+                    IsAvailable = productViewModel.IsAvailable.Equals("y") ? true : false,
+                    IsTodaySpecial = productViewModel.IsTodaySpecial.Equals("y") ? true : false,
+                    Category = productViewModel.Category,
+                    UpdatedAt=DateTime.Now,
+                    Ip = GetLocalIp(),
+                };
+                rMSDBContext.Entry(product).State=EntityState.Modified;//editing the record to the products of db context
+                rMSDBContext.SaveChanges();// actually update to the database 
+                TempData["Msg"] = "update process is completed successfully.";
+            }
+            catch(Exception e) {
+                TempData["Msg"] = "error occur when record is updated.";
+            }
+            return RedirectToAction("List");
+        }
         public string GetLocalIp() {
             var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList) {
--- RestaurantManagementSystem/Models/ViewModels/ProductViewModel.cs
+++ RestaurantManagementSystem/Models/ViewModels/ProductViewModel.cs
@@ -1,10 +1,12 @@
 ﻿namespace RestaurantManagementSystem.Models.ViewModels {
     public class ProductViewModel {
+        public string Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
         public decimal UnitPrice { get; set; }
         public string IsTodaySpecial { get; set; }
         public string IsAvailable { get; set; }
+        public int SrNo { get; set; } = 0;
     }
 }
