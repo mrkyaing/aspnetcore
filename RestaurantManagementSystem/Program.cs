17ca5f5b05646aa415dce83702c0cc03987012b8
mrkyaing <mr.kyaing7@gmail.com>
2023-08-09 21:06:27 +0630

    add AdminLTE UI.

--- RestaurantManagementSystem/Program.cs
+++ RestaurantManagementSystem/Program.cs
@@ -10,10 +10,8 @@ builder.Services.AddDbContext<RMSDBContext>(o =>
     o.UseSqlServer(config.GetConnectionString("RMSConnnectionString")));//getting the connection string from appSetting.json
 
 var app = builder.Build();
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseHttpsRedirection();
