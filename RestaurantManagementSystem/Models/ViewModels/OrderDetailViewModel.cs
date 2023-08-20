9f59378c7195ed8fdf70da13d116266f4143ce21
mrkyaing <mr.kyaing7@gmail.com>
2023-08-20 13:50:18 +0630

    add order detail function.

--- RestaurantManagementSystem/Models/ViewModels/OrderDetailViewModel.cs
+++ RestaurantManagementSystem/Models/ViewModels/OrderDetailViewModel.cs
@@ -3,8 +3,8 @@
         public string? Id { get; set; }
         public string? OrderId { get; set; }
         public string ProductId { get; set; }
+        public virtual List<ProductViewModel>? Products { get; set; }
         public int Quantity { get; set; }
         public string? Remark { get; set; }
-        public virtual List<ProductViewModel>? Products { get; set; }
     }
 }
