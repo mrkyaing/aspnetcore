eef892f78db9c69503da7e3a184e569dfe7fac95
Mg Kyaing <mgkyaing@globalwave.company>
2023-09-07 17:04:37 +0630

    nothings

--- CurrencyConvertor/Program.cs
+++ CurrencyConvertor/Program.cs
@@ -1,5 +1,10 @@
+using CurrencyConvertor.DAO;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options=>options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));
 var app = builder.Build();
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
